72c30f437461d139f3a3525cfcda31e4fcad17fc
Gommez157 <nicksss.gomes@gmail.com>
2023-11-25 22:24:36 -0300

    primeira execução do servidor

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 const express = require("express")
 
 const app = express()
+
+app.get('/', (requisicao, resposta) => {
+    resposta.send("Salve lets")
+})
+
+app.listen(3000, () => {
+    console.log("servidor rodando na porta 3000")
+})
